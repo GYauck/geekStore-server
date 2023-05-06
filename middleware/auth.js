cd211f815ebb692a686ba75909f3446e37c3cac1
GYauck <yauckgaston@gmail.com>
2023-05-06 16:54:57 -0300

    fix3

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const { validateToken } = require("../config/tokens");
 
 function validateAuth(req, res, next) {
   const token = req.headers.cookie.split("token=")[1];
-
+  console.log(req.headers)
   if (!token) return res.sendStatus(401);
 
   const { user } = validateToken(token);
